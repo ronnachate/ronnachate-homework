0d1f2c69403cfc04f6cfd372b26764a33aa3fd9d
Ronnachate <ronnachate@gmail.com>
2016-12-14 23:34:29 +0700

    Using GetContainerSize function in main program.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace Ronnahcate.Homework
 {
@@ -6,7 +7,16 @@ namespace Ronnahcate.Homework
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            if( args.Length != 1 )
+            {
+                Console.WriteLine("Please put heights as string array separate by comma!!!");
+                Console.WriteLine("eg. dotnet run 3,2,1,2,3");
+            }
+            else
+            {
+                var heightList = args[0].Split(',').Select(int.Parse).ToList();
+                Console.WriteLine("Result is " + Container.GetContainerSize(heightList));
+            }
         }
     }
 }
