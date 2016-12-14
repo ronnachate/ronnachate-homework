f797a50120c4dc676b78e59799fca6f0a0e93ef6
Ronnachate <ronnachate@gmail.com>
2016-12-14 23:05:53 +0700

    Add more test.

--- test/Container.cs
+++ test/Container.cs
@@ -1,5 +1,4 @@
 using Xunit;
-using Ronnahcate.Homework;
 using System.Collections.Generic;
 
 namespace Ronnahcate.Homework.Test
@@ -9,8 +8,24 @@ namespace Ronnahcate.Homework.Test
         [Fact]
         public void SampleDataTest()
         {
-            var sampleData = new List<int> { 0, 1, 0, 2, 1, 0, 1, 3, 2, 0, 1, 0, 2};
+            var sampleData = new List<int> {0, 1, 0, 2, 1, 0, 1, 3, 2, 0, 1, 0, 2};
             Assert.Equal(10, Container.GetContainerSize(sampleData));
+        }
+
+        [Fact]
+        public void OtherDataTest()
+        {
+            var testData = new List<int> {3,0,3};
+            Assert.Equal(3, Container.GetContainerSize(testData));
+            testData = new List<int> {3,0,1};
+            Assert.Equal(1, Container.GetContainerSize(testData));
+        }
+
+        [Fact]
+        public void EmptyListTest()
+        {
+            var testData = new List<int>();
+            Assert.Equal(0, Container.GetContainerSize(testData));
         }
     }
 }
