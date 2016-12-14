d22ab0a32fa2fbabfb9e9d2a43b59c7759cb41d1
Ronnachate <ronnachate@gmail.com>
2016-12-14 23:07:24 +0700

    Add login in funcion.

--- src/Ronnachate/Homework/Container.cs
+++ src/Ronnachate/Homework/Container.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Collections.Generic;
 
 namespace Ronnahcate.Homework
@@ -7,7 +6,38 @@ namespace Ronnahcate.Homework
     public class Container
     {
         public static int GetContainerSize( List<int> blockHeights) {
-            return 0;
+            int totalContain = 0;
+            int startingEdge = 0;
+            int blockContain = 0;
+            if( blockHeights.Count > 0 ) {
+                //looping input block, reverse to make remove command work while looping
+               foreach( var height in blockHeights.Reverse<int>()  ) {
+                   blockHeights.Remove(height);
+                   //get max height left
+                   var maxHeigthLeft  = blockHeights.OrderByDescending( h => h).FirstOrDefault();
+                   if( height >= startingEdge ) {
+                        // sum current section in to total
+                        totalContain = totalContain + blockContain;
+                        //clear section , prepare for new section
+                        blockContain = 0;
+                        //define edge for sction
+                        if( height <= maxHeigthLeft ) {
+                             startingEdge = height;
+                        }
+                        else {
+                            startingEdge = maxHeigthLeft;
+                        }
+                   }
+                   else {
+                       // add block in currect section
+                       blockContain =  blockContain +( startingEdge - height);  
+                   }
+               }
+               return totalContain;
+            }
+            else {
+                return 0;
+            }
         }
     }
 }
