86e0d1f73c94e9bd86b56ced653b9e56b18f1bbb
Ronnachate <ronnachate@gmail.com>
2016-12-14 23:24:23 +0700

    Add function description

--- src/Ronnachate/Homework/Container.cs
+++ src/Ronnachate/Homework/Container.cs
@@ -8,26 +8,26 @@ namespace Ronnahcate.Homework
         /// <summary>
         /// Calculate block contain as in array
         /// </summary>
-        ///<param name="blockHeights">list of height for container size calculate</param>
-        ///<returns>count of block contain</returns>
+        /// <param name="blockHeights">list of height for container size calculate</param>
+        /// <returns>count of block contain</returns>
         public static int GetContainerSize( List<int> blockHeights) {
             int totalContain = 0;
             int startingEdge = 0;
             int blockContain = 0;
             if( blockHeights.Count > 0 ) {
-                //looping input block, reverse to make remove command work while looping
+                // looping input block, reverse to make remove command work while looping
                foreach( var height in blockHeights.Reverse<int>()  ) {
                    blockHeights.Remove(height);
-                   //get max height left
+                   // get max height left
                    var maxHeigthLeft  = blockHeights.OrderByDescending( h => h).FirstOrDefault();
-                   //height >= startingEdge, there are starting edge and ending edge already
+                   // height >= startingEdge, there are starting edge and ending edge already
                    // So put count of block i nthis section
                    if( height >= startingEdge ) {
                         // sum current section in to total
                         totalContain = totalContain + blockContain;
-                        //clear section , prepare for new section
+                        // clear section , prepare for new section
                         blockContain = 0;
-                        //define edge for new section
+                        // define edge for new section
                         if( height <= maxHeigthLeft ) {
                              startingEdge = height;
                         }
